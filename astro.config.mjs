105a6a1a212e28643777c2fefc1b67839333188c
Barder_Lucas <bardey.estelle1@gmail.com>
2025-02-05 10:21:40 +0100

    Ajout d'un composant Greeting avec des messages aléatoires, intégration de Preact, et mise à jour des styles de navigation et de thème.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {enabled: false},
-    site: "https://astro-decouverte.netlify.app",
+  devToolbar: {enabled: false},
+  site: "https://astro-decouverte.netlify.app",
+  integrations: [preact({ compat: true })],
 });
